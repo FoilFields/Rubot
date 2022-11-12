684aa9f4db4fda099e62371d866737a270e1820c
woukie <lucaseldon@live.com>
2022-11-12 15:12:03 +0000

    logging in code

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log("Robit bot ready!")
+});
+
+client.login("MTA0MDY4MTk0NzUyNzEzOTQwOQ.GbrTlA.QMYwkg5CZn7oU3HMaDbXEI7faNSRNYnBk75G6o");
